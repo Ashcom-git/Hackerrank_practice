7dd8132f6c8e8bf9fb572735bb81adbb91a626ec
Ashcom-git <83799967+Ashcom-git@users.noreply.github.com>
2021-07-28 15:00:39 +0530

    Add files via upload

---
+++ 2D Array - DS.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(a):
+    # Write your code here
+    abc = []
+    for i in range(4):
+        for j in range(4):
+            abc.append(a[i][j]+a[i][j+1]+a[i][j+2]+a[i+1][j+1]+a[i+2][j]+a[i+2][j+1]+a[i+2][j+2])
+    return max(abc)  
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
